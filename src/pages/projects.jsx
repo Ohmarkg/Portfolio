bd140afb5eaf95c7495073b75275af4b3afc49f9
Ohmarkg <123850833+Ohmarkg@users.noreply.github.com>
2025-06-18 03:45:26 -0500

    intial layout and responsiveness

---
+++ src/pages/projects.jsx
@@ -0,0 +1,45 @@
+
+import '../styles/projects.css'
+
+
+const Projects = () => {
+    return (
+        <body>
+            <aside>
+                <h1>My hits</h1>
+                <h2>My hits</h2>
+            </aside>
+            <div className='music-player'>
+                <main>
+                    <div className='music-cover'>
+                        <div className='album-cover'>
+                            <img src='image 8.svg'></img>
+                        </div>
+                        
+                        <div className='description'>
+                            <section id ="name">
+                                <h1> Tech stack</h1>
+                                <p> React Native</p>
+                            </section>
+                            <section id = "info">
+                                <h1>Description</h1>
+                                <p>Mobile application for the TAMU SHPE organization. 
+                                    Serves 400+ members acting as central hub.
+                                </p>
+                            </section>
+                        </div>
+
+                    </div>
+
+
+                </main>
+                <footer>
+                    footer
+                </footer>
+            </div>
+
+        </body>
+    )
+}
+
+export default Projects
